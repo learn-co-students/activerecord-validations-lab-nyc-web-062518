222d1fe49ddceabc473e18ae755cacb49f7f6f3c
jaredianmills <jaredianmills@gmail.com>
2018-07-22 14:40:46 -0400

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
+class ClickbaitValidator < ActiveModel::Validator
+
+  def validate(record)
+    if record.title
+      unless ["Won't Believe", "Secret", "Top", "Guess"].any? {|word| record.title.include?(word)}
+        record.errors[:title] << "Your title is not clickbait!"
+      end
+    end
+  end
+
+end
+
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: {minimum: 250}
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
+  include ActiveModel::Validations
+  validates_with ClickbaitValidator
+
 end
