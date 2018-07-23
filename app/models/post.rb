f32182c647b8c17dbd3dfcf4e589ddb05cedacf4
Daniel Ran <danielr1735@gmail.com>
2018-07-23 09:35:11 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,14 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validate :category_within_options
+  validate :category, inclusion: { is: ('Fiction' || 'Non-Fiction'}
   validate :title_is_clickbaity
 
-  def category_within_options
-      if !(category == 'Fiction' || category == 'Non-Fiction')
-        errors.add(:category, "has to be either 'Fiction' or Non-Fiction'")
-      end
-  end
+  # def category_within_options
+  #     if !(category == 'Fiction' || category == 'Non-Fiction')
+  #       errors.add(:category, "has to be either 'Fiction' or Non-Fiction'")
+  #     end
+  # end
 
   def title_is_clickbaity
     clickbaits = ["Won't Believe", "Secret", "Top", "Guess"]
